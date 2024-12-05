10f6aaa0bc91200d4e926bbf0c7eb7619a5adf34
NarmalaSk <shashinarmala29@gmail.com>
2024-12-05 21:47:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Serve static files if needed (e.g., CSS, images)
 app.use(express.static(path.join(__dirname, 'Frontend')));
