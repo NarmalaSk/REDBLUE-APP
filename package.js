58f382cc9a221467b1d455a0fa49caa863a210ee
NarmalaSk <shashinarmala29@gmail.com>
2024-12-08 18:28:43 +0530

    update

--- package.js
+++ package.js
@@ -1,23 +1,22 @@
 {
-    "dependencies": {
-      "@redis/client": "^1.6.0",
-      "axios": "^1.7.9",
-      "express": "^4.21.2",
-      "pg": "^8.13.1",
-      "redis": "^4.7.0",
-      "redis-server": "^1.2.2",
-      "redisclient": "^0.0.8"
-    },
-    "name": "buttons-express-app",
-    "version": "1.0.0",
-    "main": "server.js",
-    "scripts": {
-      "test": "echo \"Error: no test specified\" && exit 1",
-      "start": "node server.js"
-    },
-    "keywords": [],
-    "author": "",
-    "license": "ISC",
-    "description": ""
-  }
-  
+  "dependencies": {
+    "@redis/client": "^1.6.0",
+    "axios": "^1.7.9",
+    "express": "^4.21.2",
+    "pg": "^8.13.1",
+    "redis": "^4.7.0",
+    "redis-server": "^1.2.2",
+    "redisclient": "^0.0.8"
+  },
+  "name": "buttons-express-app",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server.js"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": ""
+}
